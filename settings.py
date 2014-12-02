dd0468b67995e28b3020351dc87f37506c2ea1b1
Arbin Bulaybulay <arbinbulai@gmail.com>
2014-12-02 22:40:47 +0800

    Modified settings

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pysoldev_pysoldevco',                      # Or path to database file if using sqlite3.
-        'USER': 'pysoldev_company',                      # Not used with sqlite3.
+        'NAME': 'dbname_here',                      # Or path to database file if using sqlite3.
+        'USER': 'username_here',                      # Not used with sqlite3.
         'PASSWORD': '****',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
